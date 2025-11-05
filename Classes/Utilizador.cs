dac69e714b7062c9e191156f9c3c8e58b5e00cc7
Robinson <adrianacapela09@gmail.com>
2025-11-05 16:48:00 +0000

    Utilizador

---
+++ Classes/Utilizador.cs
@@ -0,0 +1,29 @@
+ using System;
+
+namespace ProjetoFinal.Classes
+{
+    public class Utilizador
+    {
+        //Atributos
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Perfil { get; set; }
+
+        public void Utilizador (SistemaGestaoFinancas sistema, Utilizador utilizador)
+        {
+            Console.WriteLine($"{Nome} está a registar uma nova despesa...");
+            sistema.AnalisarDespesa(despesa);
+        }
+    
+
+
+            public Utilizador(string nome, string email, string password, string perfil)
+        {
+            Nome = nome;
+            Email = email;
+            Password = password;
+            Perfil = perfil;
+        }
+    } 
+}
