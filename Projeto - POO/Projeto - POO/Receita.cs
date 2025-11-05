cde1b8c439aff249a9edc00a0dd07d0d3119ad34
barbarasantos31072 <pv31072@alunos.estgv.ipv.pt>
2025-11-05 15:33:06 +0000

    commit inicial

---
+++ Projeto - POO/Projeto - POO/Receita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto___POO
+{
+    public class Receita
+    {
+        private int Id { get; set; }
+        private string Descricao { get; set; }
+        private float Valor { get; set; }
+        private DateTime Data { get; set; }
+        private string Categoria { get; set; }
+
+    }
+}
