a6b5545c5895a0eca7d02a6c4e40bf84125bdb90
barbarasantos31072 <pv31072@alunos.estgv.ipv.pt>
2025-11-05 15:45:25 +0000

    teste commit

---
+++ Receita.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ProjetoFinal.Classes
+{
+    public class Receitas
+    {
+        // Atributos
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public float Valor { get; set; }
+        public DateTime Data { get; set; }
+        public string Categoria { get; set; }
+
+        // Construtor
+        public Receitas(int id, string descricao, float valor, DateTime data, string categoria)
+        {
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
+            Data = data;
+            Categoria = categoria;
+        }
+
+        // Método
+        public bool ValidarValor()
+        {
+            // retorna true se o valor for positivo
+            return Valor > 0;
+        }
+    }
+}
