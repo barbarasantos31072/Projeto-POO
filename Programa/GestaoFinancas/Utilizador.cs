0f90693ea82dc5954df2fbd4d347e3e25541fd16
Robinson <adrianacapela09@gmail.com>
2025-11-06 22:52:36 +0000

    utilizador

--- Programa/GestaoFinancas/Utilizador.cs
+++ Programa/GestaoFinancas/Utilizador.cs
@@ -9,7 +9,7 @@ namespace ProjetoFinal.Classes
         public string Email { get; set; }
         public string Password { get; set; }
         public string Perfil { get; set; }       
-
+        // Construtor
             public Utilizador(string nome, string email, string password, string perfil)
         {
             Nome = nome;
