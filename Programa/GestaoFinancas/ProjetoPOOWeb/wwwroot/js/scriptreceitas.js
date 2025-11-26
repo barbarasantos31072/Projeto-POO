bf6a9d6b4072256c00d69ff49130b60a4cb57310
Robinson <adrianacapela09@gmail.com>
2025-11-26 14:58:41 +0000

    receitas

---
+++ Programa/GestaoFinancas/ProjetoPOOWeb/wwwroot/js/scriptreceitas.js
@@ -0,0 +1,21 @@
+function addDespesa() {
+    let desc = document.getElementById("descricao").value;
+    let valor = document.getElementById("valor").value;
+
+    if (desc.trim() === "" || valor.trim() === "") {
+        alert("Preenche a descrição e o valor!");
+        return;
+    }
+
+    let tabela = document.getElementById("tabelaReceitas").querySelector("tbody");
+
+    let row = tabela.insertRow();
+    row.innerHTML = `
+        <td>${desc}</td>
+        <td>${parseFloat(valor).toFixed(2)} €</td>
+        <td><button class="btn-del" onclick="removeReceita(this)">Remover</button></td>
+    `;
+
+    document.getElementById("descricao").value = "";
+    document.getElementById("valor").value = "";
+}
